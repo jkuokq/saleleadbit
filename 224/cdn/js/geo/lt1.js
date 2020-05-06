0507ee77d9357a0df857ec4b100f6fb14f2af7ae
jkuokq <64432759+jkuokq@users.noreply.github.com>
2020-05-06 12:06:12 +0300

    Add files via upload

---
+++ 224/cdn/js/geo/lt1.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        lt: {
+            name: 'Lietuva',
+            code: 'lt',
+            price: 39,
+            oldPrice: 78,
+            labelPrice: 'EUR',
+            phoneHelper: 'Pavyzdžiui, +370 12345678',
+            nameHelper: 'Laimo Daukanto',
+            nameError: 'Užpildymui būtinas vardas',
+            phoneError: 'Užpildymui būtinas telefonas',
+            countryError: 'Užpildymui būtinas šalis',
+            isDefault: true
+        }
+    };
+
+})();
